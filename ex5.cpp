41275633dcb5f169426f38e21e5d2bbfa2d48753
unknown <nkahambu>
2023-11-16 19:22:55 +0200

    second commit

---
+++ ex5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//write a program in c to display the cube of the number up to an integer.
+//imput number of terms:5
+
+int main()
+{
+	int n,i;
+	int number=0;
+	cout<<"enter the number of terms:";
+	cin>>"the numbers
+	
+	for(i=0;i<=5;i++)
+cout<<"i:"<<endl;
+	
+}
+
+return 0;
